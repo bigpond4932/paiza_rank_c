d1b8fd31c3199ae8da1325bdf702a6890278a5d7
bigpond4932 <hyt4932@gmail.com>
2024-06-17 09:44:08 +0900

    Update B038.java
    1. 단일 반복문으로 처리할 수 없을지 잘 생각해봐야 한다.
    ( 즉 For문 중첩시킬 때 신중해라 )
    2. 주어진 조건 간의 관계를 생각을 하자. 거북이의 수는 학의 수에 따라서 결정된다.

--- src/b_class/B038.java
+++ src/b_class/B038.java
@@ -7,18 +7,18 @@ public class B038 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // TODO 동물원에 존재하는 파이자 학과 거북이의 수를 계산하는 프로그램을 만들어라.
-        int totalRegs = sc.nextInt(); // 학과 거북이의 다리수의 합
-        int totalNum = sc.nextInt(); // 학과 거북이의 마리수의 합
-        int regByCrane = sc.nextInt(); // 학의 다리 수
-        int regByTurtle = sc.nextInt(); // 거북이의 다리 수
+        int totalLegs = sc.nextInt(); // 학과 거북이의 다리수의 합
+        int totalAnimals = sc.nextInt(); // 학과 거북이의 마리수의 합
+        int legsPerCrane = sc.nextInt(); // 학의 다리 수
+        int legsPerTurtle = sc.nextInt(); // 거북이의 다리 수
 
         // TODO 내가 무엇을 할 것이냐. 학의 수를 늘려가면서 총 다리 수의 합의 조건을 만족하는 학과 거북이의 수를 찾아갈 것이다.
-        findAppropriateCombination result = getFindAppropriateCombination(totalNum, regByTurtle, regByCrane, totalRegs);
+        AnimalCounts result = findAnimalCounts(totalAnimals, legsPerTurtle, legsPerCrane, totalLegs);
         // TODO 결과는 두 가지.
         printAnswer(result);
     }
 
-    private static void printAnswer(findAppropriateCombination result) {
+    private static void printAnswer(AnimalCounts result) {
         if (result.caseCount() != 1){
             System.out.println("miss");
         }else{
@@ -26,28 +26,26 @@ public class B038 {
         }
     }
 
-    private static findAppropriateCombination getFindAppropriateCombination(int totalNum, int regByTurtle, int regByCrane, int totalRegs) {
-        int numOfCranes = 0;
-        int numOfTurtles = 0;
-        int caseCount = 0;
-        for(int numOfCrane = 1; numOfCrane < totalNum; numOfCrane++ ){ // 학의 최대수는 total - 1
-            for(int numOfTurtle = 1; numOfCrane + numOfTurtle <= totalNum; numOfTurtle++){  // 학과 거북이의 최대수는 total
-                if( numOfTurtle * regByTurtle + numOfCrane * regByCrane == totalRegs && numOfTurtle + numOfCrane == totalNum){ // 마리 수 and 다리 수를 만족하는 조합을 찾는다.
-                    numOfCranes = numOfCrane;
-                    numOfTurtles = numOfTurtle;
-                    caseCount++;
-                }
-                if (caseCount > 1){
-                    break;
-                }
+    private static AnimalCounts findAnimalCounts(int totalNum, int legsPerTurtle, int legsPerCrane, int totalLegs) {
+        int numCranes = 0;
+        int numTurtles = 0;
+        int solutionCount = 0;
+        for(int numCrane = 1; numCrane < totalNum; numCrane++ ){ // 학의 최대수는 total - 1
+            // 학의 수가 정해지면 거북이 수는 생각할 필요도 없이 정해진다.
+            int numTurtle = totalNum - numCrane;
+            // 이중 for문 삭제 && 총합을 계산하던 조건문 삭제!
+            if(numTurtle * legsPerTurtle + numCrane * legsPerCrane == totalLegs){
+                numCranes = numCrane;
+                numTurtles = numTurtle;
+                solutionCount++;
             }
-            if (caseCount > 1){
+            if (solutionCount > 1){
                 break;
             }
         }
-        return new findAppropriateCombination(numOfCranes, numOfTurtles, caseCount);
+        return new AnimalCounts(numCranes, numTurtles, solutionCount);
     }
 
-    private record findAppropriateCombination(int numOfCranes, int numOfTurtles, int caseCount) {
+    private record AnimalCounts(int numOfCranes, int numOfTurtles, int caseCount) {
     }
 }
