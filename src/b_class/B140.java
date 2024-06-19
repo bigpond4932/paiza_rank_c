aa8722eae6a0c6d1b9e0d51525fffef04a10ac48
bigpond4932 <hyt4932@gmail.com>
2024-06-19 09:54:48 +0900

    Update B140.java
    정리
    1. Integer형태로 int배열을 박싱하려면 stream을 취득후 boxed() 매서드를 이용하자.
    2. 이차원 배열을 1차원 배열로 바꾸는 방법은 두 가지 (For문, stream API)
    
    직렬화 : 객체를 DB에 저장하거나, 네트워크를 통해서 전송하기 위해 바이트 코드로 변환하는 작업을 말한다.
    
    stream API (java 8) : 배열이나 객체를 stream으로 변환하여 함수형 프로그래밍 스타일로 처리할 수 있게 하는 것
    
    flatMap(함수형 인터페이스) : 여러 스트림을 하나의 스트림으로 병합한다.
    
    함수형 인터페이스 : 입력타입 T -> 반환타입 R
    
    메서드레퍼런스 : 클래스명 :: 매서드 ( 예시 : Arrays :: stream )
    람다 : ( arr -> Arrays.stream(arr) ) 요런식으로 코드 작성하는 것

--- src/b_class/B140.java
+++ src/b_class/B140.java
@@ -1,38 +1,53 @@
 package b_class;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class B140 {
     // B140:ネズミ駆除
         static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        //　가지고 있는 덫으로 최대한 많은 쥐를 잡고 싶어요
         // 각 구획에 출현하는 쥐의 숫자를 카운팅
-        int[][] divWithMouse = new int[sc.nextInt()][sc.nextInt()];
-        int totalMouses = sc.nextInt();
-        int numOfTraps = sc.nextInt();
-        placeMouses(divWithMouse, totalMouses);
-        // 모든 구획을 하나의 배열에 담고 내림차순으로 정렬
-        int[] mouseNumByPlace = Arrays.stream(divWithMouse).flatMapToInt(Arrays::stream).toArray();
-        // Q. mapToInt 랑 flatMapToInt랑 뭐가 다르누?
-        Arrays.sort(mouseNumByPlace); // 내림차순 정렬이 인트배열이여야 가능하구나.
-//        System.out.println(Arrays.toString(mouseNumByPlace));
+        int rows = sc.nextInt(); // 이렇게 정의하는 것이 명확하다.
+        int cols = sc.nextInt();
+        int[][] div = new int[rows][cols];
+        int totalMouse = sc.nextInt();
+        int totalTraps = sc.nextInt();
+
+        // 쥐를 배치
+        placeMouses(div, totalMouse); // 배열의 참조를 전달하기 때문에 리턴 안해줘도 div에 쥐를 채워넣을 수 있다.
+
+        // 각 구획에 존재하는 쥐의 수를 하나의 배열에 담고 내림차순으로 정렬
+        Integer[] mouseNumByPlace = Arrays.stream(div).flatMapToInt(Arrays::stream).boxed().toArray(Integer[]::new);
+//        IntStream flatIntStream = Arrays.stream(div).flatMapToInt(array -> Arrays.stream(array)); {1234567} 와 같이 2차원 배열을 평탄화.
+
+        Arrays.sort(mouseNumByPlace, Collections.reverseOrder()); // 내림차순 정렬이 래퍼클래스 배열이여야 가능하구나. -> 이차원 배열을 Integer형의 일차원 배열로 변환하는 방법은?
+//        System.out.println(Arrays.toString(mouseNumByPlace)); // for DEBUG
+
+        // Q. mapToInt 랑 flatMapToInt랑 뭐가 다르지?
+        // Q. 애초에 stream이란게 뭘까
         // 쥐 덫의 개수만큼 더하기
-        int capturedCount = 0;
-        for(int i = 1; i <= numOfTraps; i++){ // 덫이 할당된 공간보다 더 많을 경우도 처리를 해줘야하는구나.
-            try{
-                // 이렇게 작성하는 것 보다, 덫의 수가 공간의 수보다 클 경우, 공간에 존재하는 쥐의 수를 다 더하면 되겠네.
-                capturedCount += mouseNumByPlace[mouseNumByPlace.length - i];
-            }catch (ArrayIndexOutOfBoundsException e){
-                break;
-            }
-        }
+        int capturedCount = countHunted(mouseNumByPlace, totalTraps);
+
         System.out.println(capturedCount);
     }
 
-    private static void placeMouses(int[][] divWithMouse, int totalMouses) {
-        for(int i = 0; i < totalMouses; i++){
+    private static int countHunted(Integer[] mouseNumByPlace, int numOfTraps) {
+        int result = 0;
+        if(numOfTraps >= mouseNumByPlace.length){
+            result = Arrays.stream(mouseNumByPlace).reduce(0, Integer::sum); //
+        }else{
+            for(int i = 0; i < numOfTraps; i++){
+                // 이렇게 작성하는 것 보다, 덫의 수가 공간의 수보다 클 경우, 공간에 존재하는 쥐의 수를 다 더하면 되겠네.
+                result += mouseNumByPlace[i];
+            }
+        }
+        return result;
+    }
+
+    private static void placeMouses(int[][] divWithMouse, int totalMouse) {
+        for(int i = 0; i < totalMouse; i++){
             int r = sc.nextInt()-1;
             int c = sc.nextInt()-1;
             if(divWithMouse[r][c] == 0){
